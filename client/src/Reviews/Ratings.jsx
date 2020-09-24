591953b4abb4a3d3889f2435711261f7fa79660b
Emily White <13emwhite13@gmail.com>
2020-09-24 17:29:12 -0500

    SetUp Review Form structure

---
+++ client/src/Reviews/Ratings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Ratings = () => {
+  return (
+  <div>
+    {/* <p>this is the Ratings component</p> */}
+  </div>
+  )
+}
+
+export default Ratings;
--- client/src/Reviews/Reviews.jsx
+++ client/src/Reviews/Reviews.jsx
@@ -8,5 +8,4 @@ const Reviews = () => {
   )
 }
 
-
 export default Reviews;
