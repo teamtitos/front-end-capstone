70bfb074d6274618ecbbbc50f1f7e894c3e73c80
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-09-24 16:59:48 -0500

    Set up get request for product data

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header/Header.jsx';
 import ProductView from './ProductView/ProductView.jsx';
 import RelatedProducts from './RelatedProducts/RelatedProducts.jsx';
@@ -8,15 +9,27 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-
+      productData: []
     };
+  }
+
+  componentDidMount() {
+    axios.get('http://18.224.37.110/products')
+      .then(result => {
+        this.setState({ productData: result.data }, () => {
+          console.log(this.state.productData)
+        });
+      })
+      .catch(error => {
+        console.error('There was an error with the GET request.')
+      })
   }
 
   render() {
     return (
       <div className="App">
         <Header />
-        <ProductView />
+        <ProductView productData={this.state.productData} />
         <RelatedProducts />
         <Reviews />
       </div>
