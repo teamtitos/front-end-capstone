6075ccd2c0fad398bfb040a3e049cdcd2caccb99
Emily White <13emwhite13@gmail.com>
2020-09-24 16:33:07 -0500

    ReviewsList component basic set up

--- client/src/Reviews/ReviewsList.jsx
+++ client/src/Reviews/ReviewsList.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 const ReviewsList = () => {
   return (
-  <div>
-    {/* <p>this is the ReviewsList component</p> */}
-  </div>
+  // <div>
+  //   <p>this is the ReviewsList component</p>
+  //   <p>number of total reviews, sorted drop down menu</p>
+  //   <p>stars</p> <p>username, date of review</p>
+  //   <p>review title 1</p>
+  //   <p>review body</p>
+  //   <p>recommend</p>
+  //   <p>response to review</p>
+  //   <p>rating helpfulness</p>
+
+  //   <p>number of total reviews, sorted drop down menu</p>
+  //   <p>stars</p> <p>username, date of review</p>
+  //   <p>review title 2</p>
+  //   <p>review body</p>
+  //   <p>recommend</p>
+  //   <p>response to review</p>
+  //   <p>rating helpfulness</p>
+
+  //   <button>MORE REVIEWS</button> <button>ADD A REVIEW</button>
+  // </div>
   )
 }
 
