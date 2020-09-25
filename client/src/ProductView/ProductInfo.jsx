d05b002d79f87f46226e2dc4842374577cdcdd9f
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-09-25 11:18:14 -0500

    Render product information to page

--- client/src/ProductView/ProductInfo.jsx
+++ client/src/ProductView/ProductInfo.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
-// import {useState, useEffect} from 'react';
 
-// reviews, title, category and price
 const ProductInfo = (props) => {
-  // const [details] = useState(props.details);
 
-  // useEffect(() => {
-  //   console.log(details)
-  // });
-
-  // console.log(props);
   return (
     <div>
       <p>Stars, read all reviews</p>
-      <p></p>
-      <p>{ props.details.category === undefined ? 'Loading' : props.details.category }</p>
-      {/* <p>{props.details.name}</p>
-      <p>{props.details.default_price}</p> */}
+      { !props.details
+        ? <p>Loading</p>
+        : <div>
+            <p>{ props.details.category }</p>
+            <p>{ props.details.name }</p>
+            <p>{ props.details.default_price }</p>
+          </div>
+      }
     </div>
   );
 }
--- client/src/ProductView/ProductView.jsx
+++ client/src/ProductView/ProductView.jsx
@@ -11,7 +11,7 @@ const ProductView = (props) => {
     <Row>
       <ProductImage />
       <ProductDetailsColumn details={props.productData[0]} />
-      <ProductDescription />
+      <ProductDescription details={props.productData[0]}/>
     </Row>
   );
 }
