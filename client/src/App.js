39a25c79750fd0c296360b40fe096976277536bb
Emily White <13emwhite13@gmail.com>
2020-09-25 11:06:43 -0500

    working get request for product 1 reviews

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,37 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      productData: []
+      productData: [],
+      reviewData: {}
     };
+
+    this.getReviewData = this.getReviewData.bind(this)
   }
 
   componentDidMount() {
     axios.get('http://18.224.37.110/products')
       .then(result => {
-        this.setState({ productData: result.data });
+        this.setState({ productData: result.data }, () => {
+          console.log('newProductData state:', this.state.productData)
+        });
       })
       .catch(error => {
         console.error('There was an error with the GET request.')
       })
+
+      this.getReviewData(1)
+  }
+
+  getReviewData(id) {
+    axios.get(`http://18.224.37.110/reviews/?product_id=${id}`)
+    .then(result => {
+      this.setState({reviewData: result.data}, () => {
+        console.log('newReviewData state:', this.state.reviewData)
+      })
+    })
+    .catch(error => {
+      console.log('error getting review data')
+    })
   }
 
   render() {
