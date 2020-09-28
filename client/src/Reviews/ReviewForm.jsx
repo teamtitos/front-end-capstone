3fe6f948295fabd509e7a362b9d5ce8298ff617d
Emily White <13emwhite13@gmail.com>
2020-09-28 15:29:51 -0500

    made progress on the review form

--- client/src/Reviews/ReviewForm.jsx
+++ client/src/Reviews/ReviewForm.jsx
@@ -1,49 +1,70 @@
 import React from 'react';
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 const ReviewForm = () => {
   return (
   <div>
-    {/* <p>this is the ReviewForm component</p> */}
-    {/* <form>
+    <h3>this is the ReviewForm component:</h3>
+
+    <form>
 
       <label>
-      Overall rating
-      <input type='text' />
+      Overall rating (star selector)
       </label>
+      <br></br>
 
-      <label>
-      Do you recommend this product?
-      <input type='text' />
-      </label>
+      Do you recommend this product? (radio buttons, yes or no)
+      <br></br>
+      <input type='radio' id='yes' name='recommend' value='yes' />
+      <label for='yes'>yes</label>
 
-      <label>
-      Characteristics
-      <input type='text' />
-      </label>
+      <input type='radio' id='no' name='recommend' value='no' />
+      <label for='no'>no</label>
 
-      <label>
-      Characteristics
-      <input type='text' />
-      </label>
+      <br></br>
 
-      <label>
-      Review body
-      <input type='text' />
-      </label>
+      Characteristics (radio buttons, 1-5, make a table for characteristics chart?)
+      <br></br>
+      <input type='radio' id='1' name='characteristics' value='1' />
+      <label for='1'>1</label>
 
-      <label>
-      What is your nickname
-      <input type='text' />
-      </label>
+      <input type='radio' id='2' name='characteristics' value='2' />
+      <label for='2'>2</label>
 
-      <label>
-      Your email
-      <input type='text' />
-      </label>
+      <input type='radio' id='3' name='characteristics' value='3' />
+      <label for='3'>3</label>
 
+      <input type='radio' id='4' name='characteristics' value='4' />
+      <label for='4'>4</label>
+
+      <input type='radio' id='5' name='characteristics' value='5' />
+      <label for='5'>5</label>
+
+      <br></br>
+
+      Review body (A text input allowing up to 1000 characters. Placeholder text should read: “Why did you like the product or not?”., minimum of 50 characters)
+      <br></br>
+      <textarea rows='5' cols='35' minlength='50' maxlength='1000' placeholder='Why did you like the product or not?'></textarea>
+
+      <br></br>
+
+      What is your nickname (A text input allowing up to 60 characters for the user’s display name Placeholder text should read: “Example: jackson11!”.)
+      <br></br>
+      <input type='text' maxlength='60' placeholder='Example: jackson11!'/>
+
+      <br></br>
+
+      Your email (A text input allowing up to 60 characters. Placeholder text should read: “Example: jackson11@email.com”.)
+      <br></br>
+      <input type='email' maxlength='60' placeholder='Example: jackson11@email.com'/>
+
+      <br></br>
       <input type='submit' value='Submit Review' />
 
-    </form> */}
+    </form>
+
   </div>
   )
 }
--- client/src/Reviews/Reviews.jsx
+++ client/src/Reviews/Reviews.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReviewsList from './ReviewsList.jsx';
+import ReviewForm from './ReviewForm.jsx';
 
 const Reviews = (props) => {
   console.log('props from app:', props)
-  
+
   return (
   <div>
     <h3>Reviews Component:</h3>
     <ReviewsList reviewInfo={props.reviewData} reviewDetails={props.reviewData.results}/>
+    <ReviewForm />
   </div>
   )
 }
