f2f1fa8da17b153b07e94e55d7dd0e73c176994a
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-09-28 11:34:04 -0500

    Display first full resolution image from current product style

--- client/src/ProductView/ProductImage.jsx
+++ client/src/ProductView/ProductImage.jsx
@@ -1,24 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image'
 
 const ProductImage = (props) => {
   const [stylesList, setStyles] = useState([]);
-  const [currentStyle, setCurrentStyleId] = useState(2);
-  const [photosList, setPhotos] = useState([]);
+  const [currentStyle, setCurrentStyleId] = useState(4);
 
   useEffect(() => {
     setStyles(props.styles.results);
-    if (stylesList && stylesList.length) {
-      console.log(stylesList[currentStyle])
-    }
+    console.log(stylesList)
   });
 
 
   return (
     <Col sm={8}>
       { stylesList && stylesList.length
+        ? <div><Image src={stylesList[currentStyle].photos[0].url} fluid></Image></div>
+        : <div>Loading...</div>
+      }
+      { stylesList && stylesList.length
           ? <div>{stylesList[currentStyle].photos.map(photo => {
-              return <img src={photo.thumbnail_url}></img>
+              return <Image src={photo.thumbnail_url}></Image>
             })}</div>
           : <div>Loading...</div>
       }
