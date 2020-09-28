3ca599a68ffe9e20661dfc4ab77723c35e992d0d
Emily White <13emwhite13@gmail.com>
2020-09-28 10:42:04 -0500

    Most of the review tile completed

--- client/src/Reviews/ReviewsList.jsx
+++ client/src/Reviews/ReviewsList.jsx
@@ -2,48 +2,33 @@ import React from 'react';
 
 const ReviewsList = (props) => {
   console.log('props from Reviews:', props)
-  // map over the reviews prop
-
-  // {!props.details
-  //   ? <p>Loading</p>
-  //   : <div>
-  //       <h4>{props.details.slogan}</h4>
-  //       <p>{props.details.description}</p>
-  //     </div>
-  // }
 
   return (
   <div>
+    { !props.reviewDetails
+      ? <p>Loading</p>
+      : <div>
+          <h3>ReviewsList Component:</h3>
+          <p>{props.reviewInfo.count} sorted by drop down menu</p>
+          <p>star rating {props.reviewDetails[0].reviewer_name} {props.reviewDetails[0].date}</p>
+          <p>review title 1 (summary?)</p>
+          <p>{props.reviewDetails[0].body}</p>
+          <p>recommend</p>
+          <p>Response:</p>
+          <p>Helpful? Yes ({props.reviewDetails[0].helpfulness}) | Report</p>
 
-  {!props.reviewDetails
-    ? <p>Loading</p>
-    : <div>
-        <p>{props.reviewDetails[0].body}</p>
+          <p>star rating {props.reviewDetails[1].reviewer_name} {props.reviewDetails[1].date}</p>
+          <p>review title 2 (summary?)</p>
+          <p>{props.reviewDetails[1].body}</p>
+          <p>recommend</p>
+          <p>Response:</p>
+          <p>Helpful? Yes ({props.reviewDetails[1].helpfulness}) | Report</p>
+
+          <button>ADD A REVIEW</button>
       </div>
-  }
-
-    <h3>ReviewsList Component:</h3>
-    <p>number of reviews sorted by drop down menu</p>
-    <p>star rating username date of review</p>
-
-    <p>review title 1</p>
-    <p>review body</p>
-    <p>recommend</p>
-    <p>response</p>
-    <p>helpfulness</p>
-
-    <p>number of reviews sorted by drop down menu</p>
-    <p>star rating username date of review</p>
-    <p>review title 2</p>
-    <p>review body</p>
-    <p>recommend</p>
-    <p>response</p>
-    <p>helpfulness</p>
-
-    <button>MORE REVIEWS </button> <button>ADD A REVIEW +</button>
+    }
   </div>
   )
 }
-
 
 export default ReviewsList;
