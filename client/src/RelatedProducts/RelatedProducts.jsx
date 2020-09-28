225260ec7b4982524e57f523493511c5d6fa15c4
gitHandle <earturoga@gmail.com>
2020-09-28 17:03:10 -0600

    Implemented related products carousel & added star rating component

--- client/src/RelatedProducts/RelatedProducts.jsx
+++ client/src/RelatedProducts/RelatedProducts.jsx
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import CarouselOutfit from './CarouselOutfit.jsx';
-import ProductCard from './ProductCard.jsx';
-import CarouselProduct from './CarouselOutfit.jsx';
-import Comparison from './Comparison.jsx';
+// import CarouselOutfit from './CarouselOutfit.jsx';
+import CarouselProduct from './CarouselProduct.jsx';
+import './RelatedProducts.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class RelatedProducts extends Component {
   constructor() {
     super()
-    this.state = {
-
-    }
+    this.state = {}
   }
   
   render() {
     return (
       <div>
-
+      <CarouselProduct />
       </div>
     )
   }
