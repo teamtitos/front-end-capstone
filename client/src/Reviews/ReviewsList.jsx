82d81edd061a1e5122f227e11d3eee57fc1e3721
Emily White <13emwhite13@gmail.com>
2020-09-29 10:40:52 -0500

    added some styling to the review tile

--- client/src/Reviews/ReviewsList.jsx
+++ client/src/Reviews/ReviewsList.jsx
@@ -4,59 +4,82 @@ const ReviewsList = (props) => {
   // console.log('props from Reviews:', props)
 
   return (
-    <div>
-  {/* <div>
-    { !props.reviewDetails
-      ? <p>Loading</p>
-      : <div>
-          <h3>ReviewsList Component:</h3>
-          <p>{props.reviewInfo.count} sorted by drop down menu</p>
-          <p>star rating {props.reviewDetails[0].reviewer_name} {props.reviewDetails[0].date}</p>
-          <p><strong>{props.reviewDetails[0].summary}</strong></p>
-          <p>{props.reviewDetails[0].body}</p>
-          <p>Helpful? Yes ({props.reviewDetails[0].helpfulness}) | Report</p>
+  <div>
+    <div class='container'>
+      { !props.reviewDetails
+        ? <p>Loading</p>
+        : <div>
+            <h3>ReviewsList Component:</h3>
 
-          <p>star rating {props.reviewDetails[1].reviewer_name} {props.reviewDetails[1].date}</p>
-          <p><strong>{props.reviewDetails[1].summary}</strong></p>
-          <p>{props.reviewDetails[1].body}</p>
-          <p>Helpful? Yes ({props.reviewDetails[1].helpfulness}) | Report</p>
+            <div class='row'>
+              <div class='col-sm'>
+                <p>{props.reviewInfo.count} sorted by drop down menu</p>
+              </div>
+            </div>
 
-          <button>ADD A REVIEW</button>
-      </div>
-    }
-  </div> */}
+            <div class='row'>
+              <div class='col-sm'>
+              <p>star rating</p>
+              </div>
+              <div class='col-sm'></div>
+              <div class='col-sm'>
+                <p> {props.reviewDetails[0].reviewer_name} {props.reviewDetails[0].date}</p>
+              </div>
+            </div>
 
-  <div class='container'>
-    <p>Pratice grid</p>
-    <div class='row'>
-      <div class='col-sm'>
-        <p>row 1 col 1 (number of reviews, sorted drop down menu)</p>
-      </div>
+            <div class='row'>
+              <div class='col-sm'>
+                <p><strong>{props.reviewDetails[0].summary}</strong></p>
+              </div>
+            </div>
+
+            <div class='row'>
+              <div class='col-sm'>
+                <p>{props.reviewDetails[0].body}</p>
+              </div>
+            </div>
+
+            <div class='row'>
+              <div class='col-sm'>
+                <p>Helpful? Yes ({props.reviewDetails[0].helpfulness}) | Report</p>
+              </div>
+            </div>
+
+        <div class='row'>
+          <div class='col-sm'>
+            <p>star rating</p>
+          </div>
+          <div class='col-sm'></div>
+          <div class='col-sm'>
+            <p>{props.reviewDetails[1].reviewer_name} {props.reviewDetails[1].date}</p>
+          </div>
+        </div>
+
+        <div class='row'>
+          <div class='col-sm'>
+            <p><strong>{props.reviewDetails[1].summary}</strong></p>
+          </div>
+        </div>
+
+        <div class='row'>
+          <div class='col-sm'>
+            <p>{props.reviewDetails[1].body}</p>
+          </div>
+        </div>
+
+        <div class='row'>
+          <div class='col-sm'>
+            <p>Helpful? Yes ({props.reviewDetails[1].helpfulness}) | Report</p>
+          </div>
+        </div>
+        </div>
+      }
     </div>
-
-    <div class='row'>
-      <div class='col-sm'>
-        <p>row 2 col 1 (star rating)</p>
-      </div>
-      <div class='col-sm'></div>
-      <div class='col-sm'>
-        <p>row 2 col 3(username, date of review)</p>
-      </div>
-    </div>
-
-    <div class='row'>
-      <div class='col-sm'>
-        <p>row 3 col 1 (review summary)</p>
-      </div>
-    </div>
-
-    <div class='row'>
-      <p>row 4 col 1</p>
-    </div>
-  </div>
-
+    <button>ADD A REVIEW</button>
   </div>
   )
 }
+
+
 
 export default ReviewsList;
