68a59f30c2f7d2220dedc0011fd42c23876196a9
gitHandle <earturoga@gmail.com>
2020-09-29 13:07:14 -0600

    Added loading spinner and add outfit function

---
+++ client/src/RelatedProducts/Loading.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Carousel from 'react-elastic-carousel';
+import Spinner from 'react-bootstrap/Spinner';
+
+const Loading = () => {
+  return (
+    <div className="main">
+    <h6 className="title font-weight-light">RELATED PRODUCTS</h6>
+    <Carousel 
+    pagination={false} showArrows={true}
+    style={{backgroundColor: 'white'}}
+    >
+    <div key={1} className="card" style={{height: "300px", borderColor: "white"}}>
+    <span className="spinner">
+    <Spinner animation="border" role="status" variant="secondary">
+    <span className="sr-only">Loading...</span>
+    </Spinner>
+    </span>
+    </div>
+    <div key={2}></div>
+    <div key={3}></div>
+    </Carousel>
+    </div>
+  )
+}
+
+export default Loading;
--- client/src/RelatedProducts/ProductCard.jsx
+++ client/src/RelatedProducts/ProductCard.jsx
@@ -8,16 +8,20 @@ const ProductCard = ({product}) => {
   return (
     <div className="card" style={{alignItems: "center"}} >
     <span style={{all: "notset"}}>
-    <img className="image" src="https://demo.sirv.com/bag.jpg?scale.width=252&scale.height=265" alt={product.name}/>
+    <img className="image" src="https://demo.sirv.com/bag.jpg?scale.width=252&scale.height=265" 
+    alt={product.name}/>
 
     {/*POP-OVER HERE*/}
-    <span className="littlestar" onClick={() => {console.log('star clicked')}} style={{position: "absolute", top: "8px",  right: "16px", fontSize: "19px"}} >&#x2606;</span>
+    <span className="littlestar" onClick={() => {console.log('star clicked')}} 
+    style={{position: "absolute", top: "8px",  right: "16px", fontSize: "19px"}}
+    >&#x2606;</span>
     </span>
     {/* POP-OVER ENDS*/}
     
     <div className="text">
       <span className="category font-weight-light">{product.category}</span> <br/>
-      <span className="font-weight-bold" style={{fontSize: "15px"}}>{product.name}</span> <br/>
+      <span className="font-weight-bold" 
+      style={{fontSize: "15px"}}>{product.name}</span> <br/>
       <span className="price" >${product.default_price}</span> <br/>
       <Stars id={product.id}/> <br/>
     </div>
