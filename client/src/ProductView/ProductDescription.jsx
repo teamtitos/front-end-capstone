29cfffe4d433786cdc5a85c3c5d4ba5d3e3e34d7
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-09-29 18:05:32 -0500

    Add features to product description

--- client/src/ProductView/ProductDescription.jsx
+++ client/src/ProductView/ProductDescription.jsx
@@ -3,15 +3,30 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const ProductDescription = (props) => {
+  console.log(props.details)
   return (
     <Row className="productDescription">
-      <Col>
+      <Col sm={7} className="description-body">
         {!props.details
           ? <p>Loading</p>
           : <div>
               <h4>{props.details.slogan}</h4>
               <p>{props.details.description}</p>
             </div>
+        }
+      </Col>
+      <Col sm={3}>
+        { !props.details.features
+            ? <p>Loading</p>
+            : props.details.features.map(item => {
+              return (
+                <div className="feature">
+                  <p>
+                    <i class="fa fa-check" aria-hidden="true"></i> {item.feature}: {item.value}
+                  </p>
+                </div>
+              );
+            })
         }
       </Col>
     </Row>
