e8614e920e70cd7771b4a780d60aaaa06b14bacf
gitHandle <earturoga@gmail.com>
2020-09-29 13:16:56 -0600

    formatted Loading file

--- client/src/RelatedProducts/Loading.jsx
+++ client/src/RelatedProducts/Loading.jsx
@@ -10,13 +10,17 @@ const Loading = () => {
     pagination={false} showArrows={true}
     style={{backgroundColor: 'white'}}
     >
-    <div key={1} className="card" style={{height: "300px", borderColor: "white"}}>
+
+    <div key={1} className="card" 
+    style={{height: "300px", borderColor: "white"}}>
     <span className="spinner">
     <Spinner animation="border" role="status" variant="secondary">
     <span className="sr-only">Loading...</span>
     </Spinner>
     </span>
     </div>
+    
+    {/*divs required here by carousel module*/}
     <div key={2}></div>
     <div key={3}></div>
     </Carousel>
