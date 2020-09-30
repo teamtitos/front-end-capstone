d6ab93238c709619175e498c376d1a2450525a10
Emily White <13emwhite13@gmail.com>
2020-09-30 12:12:17 -0500

    organized spacing and removed comments for the modal window

--- client/src/Reviews/ModalWindow.jsx
+++ client/src/Reviews/ModalWindow.jsx
@@ -2,12 +2,9 @@ import React, {useState} from 'react';
 import ReviewForm from './ReviewForm.jsx'
 
 import Modal from 'react-bootstrap/Modal'
-import ModalDialog from 'react-bootstrap/ModalDialog'
 import ModalHeader from 'react-bootstrap/ModalHeader'
 import ModalTitle from 'react-bootstrap/ModalTitle'
 import ModalBody from 'react-bootstrap/ModalBody'
-// import ModalFooter from 'react-bootstrap/ModalFooter'
-// import Button from 'react-bootstrap/Button'
 
 const ModalWindow = () => {
   const [show, setShow] = useState(false);
@@ -16,26 +13,20 @@ const ModalWindow = () => {
 
   return(
     <div>
-      {/* <p>this is the modal</p> */}
-
-      <>
       <button variant="primary" onClick={handleShow}>
         {/* Launch demo modal */}
         ADD A REVIEW
       </button>
-
+      
       <Modal show={show} onHide={handleClose}>
         <Modal.Header>
           <Modal.Title>Write Your Review</Modal.Title>
         </Modal.Header>
-
         <Modal.Body>
+          <h4>About the [Product Name Here]</h4>
           <ReviewForm />
         </Modal.Body>
-
       </Modal>
-    </>
-
     </div>
   )
 }
