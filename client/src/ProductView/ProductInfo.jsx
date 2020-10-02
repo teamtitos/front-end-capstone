d6fe9032ffa1e483ee726f74847eb57b8d795dc7
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-10-02 12:41:47 -0500

    Check if styleDetails is not undefined before checking the sale_price

--- client/src/ProductView/ProductInfo.jsx
+++ client/src/ProductView/ProductInfo.jsx
@@ -18,7 +18,7 @@ const ProductInfo = (props) => {
         : <div>
             <p className="category">{ props.details.category }</p>
             <h2 className="title">{ props.details.name }</h2>
-              { props.styleDetails.sale_price !== '0'
+              { props.styleDetails && props.styleDetails.sale_price !== '0'
                   ? <p className="price">
                       <span className="strikethrough">${ props.styleDetails.original_price }</span>
                       <span className="sale"> ${ props.styleDetails.sale_price }</span>
