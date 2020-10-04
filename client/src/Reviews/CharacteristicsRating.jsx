0858d9581d4231f0f3830b8ce311352973f5283c
Emily White <13emwhite13@gmail.com>
2020-10-04 01:18:58 -0500

    made progress on the characteristics slider

--- client/src/Reviews/CharacteristicsRating.jsx
+++ client/src/Reviews/CharacteristicsRating.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Slider from '@material-ui/core/Slider';
 
 const CharacteristicsRating = () => {
   return (
     <div>
-
+      <Slider
+      step={5}
+      />
     </div>
   )
 }
