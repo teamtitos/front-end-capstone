62bc2782575e889ddf721137a944ad0012139150
Leslie Crunkilton <lcrunkilton@gmail.com>
2020-10-06 17:18:09 -0500

    Wrap app in higher order component that tracks clicks, and post to API

---
+++ client/src/Tracker.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import axios from 'axios';
+
+function Tracker(Component) {
+
+  return class extends React.Component {
+
+    componentDidMount() {
+      this.trackClicks();
+    }
+
+    saveElement(e) {
+      let element =  e.srcElement.outerHTML.toString().split('>');
+      return element[0] + '>';
+    }
+
+    postInteraction(obj) {
+      axios.post('http://18.224.37.110/interactions', obj)
+        .then(res => {
+          console.log('created');
+        })
+        .catch(err => {
+          console.error('There was an error posting the interaction');
+        })
+    }
+
+    trackClicks() {
+      let time = new Date().toLocaleString();
+      let widget = '';
+      let element = '';
+      let data = {
+        time: time
+      };
+
+      window.addEventListener('click', (e) => {
+
+        let productView = document.querySelector('.productViewRow');
+        let relatedProductsView = document.querySelector('.relatedProducts');
+        let reviewsView = document.querySelector('.reviews');
+
+        if (productView.contains(e.target)) {
+          data["element"] = this.saveElement(e);
+          data["widget"] = 'Product Overview';
+        }
+
+        if(relatedProductsView.contains(e.target)) {
+          data["element"] = this.saveElement(e);
+          data["widget"] = 'Related Products';
+        }
+
+        if(reviewsView.contains(e.target)) {
+          data["element"] = this.saveElement(e);
+          data["widget"] = 'Reviews';
+        }
+
+        this.postInteraction(data);
+
+      })
+
+    }
+
+
+    render() {
+      return (
+        <Component />
+      );
+    }
+  }
+}
+
+export default Tracker;
+
